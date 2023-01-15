eea13c7653178a23940525cb14a33ceccaf43b51
Pasi P <pjpakkala@gmail.com>
2023-01-15 13:15:02 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+
+
+
+
+# Pull in 5 of the most recent news stories
+#Ask openai to summarise each story in two to three words.
+#Pass those words to Spotify in a search. Show and give a sample of each song.
+#optional to build this in Flask. 
+#A command line program is just fine. The console should display:
+    #The name of each track (five tracks)
+    #The prompt words used for the search
+    #The URL to get the sample
